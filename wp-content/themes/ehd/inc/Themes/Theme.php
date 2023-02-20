f96400da1bc815019b49fae1e1693ab910504857
dungnq81 <dungnq81@gmail.com>
2023-02-20 23:47:24 +0700

    20/02/2023

--- wp-content/themes/ehd/inc/Themes/Theme.php
+++ wp-content/themes/ehd/inc/Themes/Theme.php
@@ -510,5 +510,66 @@ final class Theme
             return $arr;
 
         }, 11, 1);
+
+        // -------------------------------------------------------------
+        // images sizes
+        // -------------------------------------------------------------
+
+        /**
+         * thumbnail (480x0)
+         * medium (768x0)
+         * large (1024x0)
+         *
+         * widescreen (1920x9999)
+         * post-thumbnail (1200x9999)
+         */
+
+        // custom thumb
+        add_image_size('widescreen', 1920, 9999, false);
+        add_image_size('post-thumbnail', 1200, 9999, false);
+
+        // ------------------------------------------
+
+        /**
+         * Disable unwanted image sizes
+         */
+        add_filter('intermediate_image_sizes_advanced', function ($sizes) {
+
+            unset($sizes['medium_large']);
+
+            unset($sizes['1536x1536']); // disable 2x medium-large size
+            unset($sizes['2048x2048']); // disable 2x large size
+
+            return $sizes;
+        });
+
+        // ------------------------------------------
+
+        // Disable Scaled
+        add_filter('big_image_size_threshold', '__return_false');
+
+        // ------------------------------------------
+
+        /**
+         * Disable Other Sizes
+         */
+        add_action('init', function () {
+            remove_image_size('1536x1536'); // disable 2x medium-large size
+            remove_image_size('2048x2048'); // disable 2x large size
+        });
+
+        // ------------------------------------------
+
+        add_filter('post_thumbnail_html', function ($html) {
+            return preg_replace('/(<img[^>]+)(style=\"[^\"]+\")([^>]+)(>)/', '${1}${3}${4}', $html);
+        }, 10, 1);
+
+        add_filter('image_send_to_editor', function ($html) {
+            return preg_replace('/(<img[^>]+)(style=\"[^\"]+\")([^>]+)(>)/', '${1}${3}${4}', $html);
+        }, 10, 1);
+
+        add_filter('the_content', function ($html) {
+            return preg_replace('/(<img[^>]+)(style=\"[^\"]+\")([^>]+)(>)/', '${1}${3}${4}', $html);
+        }, 10, 1);
     }
 }
